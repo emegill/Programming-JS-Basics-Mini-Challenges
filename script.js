2c60d8551453daf1d267b7b0b20cb203231590f7
Ethan Megill <megill1352@gmail.comm>
2017-04-12 17:18:41 -0400

    Q2-Q3 done

--- script.js
+++ script.js
@@ -13,13 +13,41 @@ var c = +a + +b;
 console.log(c);
 
 
-
-
-
 // 2.Create a multidimensional array related to a subject that interests you. Output two of the items in sub-arrays to the console.
 
+var toyota = ["tacoma", "camry", "corolla"];
+
+var mazda = ["rx-7", "mx-3", "mx-6"];
+
+var cars = [toyota, mazda];
+
+function printCars() {
+
+	for (var i = 0; i < 2; i++) {
+		var model = cars[i];
+		carsNames(model);
+	}
+
+}
+
+function carsNames(model) {
+	for (var i = 0; i < 2; i++) {
+		console.log(model[i]);
+	}
+}
+
+printCars();
 
 // 3.Write a script that checks if a variable captured from a prompt is less than 100. If it is, alert the user that their variable is less than 100. If it is not alert the user of what the value was and that it was greater than 100.
+
+var status = "88";
+
+if (status < 100) {
+	alert("variable is less than 100");
+} else if (status > 100)
+	alert(status + " " + "variable is greater than 100.");
+
+
 
 
 // 4.Try running the script and then changing the variable's value to see how this affects the program's output.
