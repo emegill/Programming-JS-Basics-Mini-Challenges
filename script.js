3d98d841cdd32946f4fe8b72adecff9345be52b2
Ethan Megill <megill1352@gmail.comm>
2017-04-12 16:37:54 -0400

    Q1

---
+++ script.js
@@ -0,0 +1,40 @@
+// 1.Create a script with two variables, each assigned to a number. Add them together and output the result to the console. Now do the same with two strings.
+
+var x = 1;
+var y = 2;
+var z = x + y;
+
+console.log(z);
+
+var a ="1";
+var b ="2";
+var c = +a + +b;
+
+console.log(c);
+
+
+
+
+
+// 2.Create a multidimensional array related to a subject that interests you. Output two of the items in sub-arrays to the console.
+
+
+// 3.Write a script that checks if a variable captured from a prompt is less than 100. If it is, alert the user that their variable is less than 100. If it is not alert the user of what the value was and that it was greater than 100.
+
+
+// 4.Try running the script and then changing the variable's value to see how this affects the program's output.
+
+
+// 5.Write a similar script to check if a string stored in a variable is the same as another string.
+
+
+// 6.Declare a function that takes a name as an argument and tells the user what name they've entered. Try running it after it has been declared.
+
+
+// 7.Declare a function that takes no arguments but prints something to the console. Try running it after it has been declared.
+
+
+// 8.Declare a function that depending upon which virtual 'door' was entered tells the user they've received a different 'prize' in an alert. After declaring the function, try running it with different options. There must be at least 3 doors.
+
+
+
